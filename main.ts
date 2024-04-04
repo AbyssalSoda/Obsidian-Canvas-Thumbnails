4c44f9c0aaf9f1d91b2882422e78a67a9838a35e
AbyssalSoda <you@example.com>
2024-04-04 07:11:34 -0400

    .clip support

--- main.ts
+++ main.ts
@@ -1,134 +1,166 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, Plugin, TAbstractFile, TFolder, TFile, FileSystemAdapter } from 'obsidian';
+import { promises as fs } from 'fs';
+import * as path from 'path';
+import * as xml2js from 'xml2js';
+import * as os from 'os';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ClipStudioThumbnailViewerSettings {
+    thumbnailsDirectory: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ClipStudioThumbnailViewerSettings = {
+    thumbnailsDirectory: 'thumbnails'
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ClipStudioThumbnailViewer extends Plugin {
+    settings: ClipStudioThumbnailViewerSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
+        await this.ensureDirectoryExists(this.settings.thumbnailsDirectory);
+        this.registerDomEvent(document, 'drop', (evt: DragEvent) => this.handleFileDrop(evt));
+        this.observeCanvasChanges();
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    private observeCanvasChanges() {
+        const canvasContainer = document.body;
+        const config = { childList: true, subtree: true };
+    
+        const callback = (mutationsList: MutationRecord[], observer: MutationObserver) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(async (node) => {
+                        if (!(node instanceof HTMLElement)) return;
+                        const canvasNodeContent = node.querySelector('.canvas-node-content');
+                        if (canvasNodeContent && canvasNodeContent.querySelector('.file-embed-title')?.textContent?.trim().endsWith('.clip')) {
+                            // Using ?? to return null if the value is undefined
+                            const title = canvasNodeContent.querySelector('.file-embed-title')?.textContent?.trim() ?? null;
+                            console.log('Canvas node content added:', canvasNodeContent);
+                            this.updateCanvasWithThumbnail(canvasNodeContent, title);
+                        }
+                    });
+                }
+            }
+        };
+    
+        new MutationObserver(callback).observe(canvasContainer, config);
+    }
+    
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    private async updateCanvasWithThumbnail(canvasNodeContent: Element, title: string | null) {
+        if (!title) return;
+        const fileNameWithoutExtension = title.replace('.clip', '');
+        console.log(`Attempting to update canvas for: ${fileNameWithoutExtension}`);
+    
+        const thumbnailsPath = `thumbnails/${fileNameWithoutExtension}.png`;
+        console.log(`Looking for thumbnail at path: ${thumbnailsPath}`);
+    
+        const file = this.app.vault.getAbstractFileByPath(thumbnailsPath);
+    
+        if (file instanceof TFile) {
+            console.log(`File found: ${file.path}`);
+            const resourcePath = this.app.vault.getResourcePath(file);
+            console.log(`Using resource path: ${resourcePath}`);
+            canvasNodeContent.innerHTML = `<img src="${resourcePath}" alt="${fileNameWithoutExtension}" style="max-width: 100%; max-height: 100%;">`;
+        } else {
+            console.error(`Thumbnail file not found for: ${fileNameWithoutExtension} at path: ${thumbnailsPath}`);
+        }
+    }
+    
+    
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    async handleFileDrop(evt: DragEvent) {
+        if (!evt.ctrlKey || !evt.dataTransfer) return;
+        evt.preventDefault();
+        const items = Array.from(evt.dataTransfer.items).filter(item => item.kind === 'file' && item.getAsFile()?.name.endsWith('.clip'));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        for (const item of items) {
+            const file = item.getAsFile();
+            console.log(`Processing .clip file: ${file?.name}`);
+            new Notice(`Processing .clip file: ${file?.name}`);
+            if (file) {
+                const fileNameWithoutExtension = file.name.replace('.clip', '');
+                await this.processClipFile(fileNameWithoutExtension);
+            }
+        }
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async processClipFile(fileNameWithoutExtension: string) {
+        const searchDirectory = path.join(os.homedir(), 'Documents', 'CELSYS', 'CLIPStudioCommon', 'Document');
+        console.log(`Searching in directory: ${searchDirectory}`);
+        await this.findCatalogXml(searchDirectory, fileNameWithoutExtension);
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async findCatalogXml(directory: string, targetFileNameWithoutExtension: string) {
+        let filesAndDirs;
+        try {
+            filesAndDirs = await fs.readdir(directory, { withFileTypes: true });
+        } catch (err) {
+            console.error('Error reading directory:', err);
+            return;
+        }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+        for (const dirent of filesAndDirs) {
+            if (dirent.isDirectory()) {
+                await this.findCatalogXml(path.join(directory, dirent.name), targetFileNameWithoutExtension);
+            } else if (dirent.name === 'catalog.xml') {
+                await this.parseXmlFile(path.join(directory, dirent.name), targetFileNameWithoutExtension);
+            }
+        }
+    }
 
-	onunload() {
+    async parseXmlFile(xmlFilePath: string, targetFileNameWithoutExtension: string) {
+        let xml;
+        try {
+            xml = await fs.readFile(xmlFilePath, 'utf8');
+        } catch (err) {
+            console.error('Error reading XML file:', err);
+            return;
+        }
 
-	}
+        xml2js.parseString(xml, async (err, result) => {
+            if (err) {
+                console.error('Error parsing XML:', err);
+                return;
+            }
+            const projectName = result.archive.catalog[0].name[0];
+            if (projectName === targetFileNameWithoutExtension) {
+                console.log(`Found matching project: ${projectName}`);
+                const originalThumbnailPath = path.join(path.dirname(xmlFilePath), 'thumbnail', 'thumbnail.png');
+                await this.copyThumbnailToVault(originalThumbnailPath, projectName);
+            }
+        });
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    async copyThumbnailToVault(originalThumbnailPath: string, projectName: string) {
+        const thumbnailFolderPath = this.settings.thumbnailsDirectory;
+        const newFileName = `${projectName}.png`;
+        try {
+            const data = await fs.readFile(originalThumbnailPath);
+            await this.ensureDirectoryExists(thumbnailFolderPath);
+            await this.app.vault.createBinary(`${thumbnailFolderPath}/${newFileName}`, data);
+            console.log(`Thumbnail copied to ${thumbnailFolderPath}/${newFileName}`);
+        } catch (err) {
+            console.error('Error handling thumbnail:', err);
+        }
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+    async ensureDirectoryExists(folderPath: string) {
+        let folder = this.app.vault.getAbstractFileByPath(folderPath);
+        if (!folder || !(folder instanceof TFolder)) {
+            try {
+                await this.app.vault.createFolder(folderPath);
+            } catch (err) {
+                console.error('Error creating folder:', err);
+            }
+        }
+    }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 }
